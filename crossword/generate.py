111c219488ceac8ba3860a6573c7a2c02ad3025e
TheGod0fWar <moritz.sanner@gmx.de>
2026-07-07 17:42:22 +0200

    assignment_done

--- crossword/generate.py
+++ crossword/generate.py
@@ -168,24 +168,35 @@ class CrosswordCreator():
                     
 
     def assignment_complete(self, assignment):
-        """
-        Return True if `assignment` is complete (i.e., assigns a value to each
-        crossword variable); return False otherwise.
-        """
-        assignments = []
-        for a in assignment:
-            assignments.append(a)
-            
-        self.ac3(assignments)
-        raise NotImplementedError
-
+    
+        return len(assignment) == len(self.crossword.variables)
+    
     def consistent(self, assignment):
         """
         Return True if `assignment` is consistent (i.e., words fit in crossword
         puzzle without conflicting characters); return False otherwise.
         """
-        raise NotImplementedError
-
+        assignments = []
+        for a in assignment.values():
+            if a not in assignments:
+                assignments.append(a)
+            else:
+                return False
+            
+            if len(assignment[a]) != self.crossword.variables[a].lengh:
+                return False
+        
+            neighbors = self.crossword.neighbors(a)
+            for y in neighbors:
+                if self.crossword.overlaps[y,a] != None:
+                    return False
+            
+        return True
+            
+            
+            
+        
+       
     def order_domain_values(self, var, assignment):
         """
         Return a list of values in the domain of `var`, in order by
