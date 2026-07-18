467672681619bf0340b66ecba2f766952fb49d8f
TheGod0fWar <moritz.sanner@gmx.de>
2026-07-18 17:41:39 +0200

    model

--- traffic/traffic.py
+++ traffic/traffic.py
@@ -5,6 +5,7 @@ import sys
 import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import ReLU
 
 EPOCHS = 10
 IMG_WIDTH = 30
@@ -45,20 +46,24 @@ def main():
 
 
 def load_data(data_dir):
-    """
-    Load image data from directory `data_dir`.
+    images = []
+    labels = []
 
-    Assume `data_dir` has one directory named after each category, numbered
-    0 through NUM_CATEGORIES - 1. Inside each category directory will be some
-    number of image files.
+    for category in os.listdir(data_dir):
+        category_path = os.path.join(data_dir, category)
 
-    Return tuple `(images, labels)`. `images` should be a list of all
-    of the images in the data directory, where each image is formatted as a
-    numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
-    be a list of integer labels, representing the categories for each of the
-    corresponding `images`.
-    """
-    raise NotImplementedError
+        if os.path.isdir(category_path):
+            for file_name in os.listdir(category_path):
+                file_path = os.path.join(category_path, file_name)
+
+                img = cv2.imread(file_path)
+
+                if img is not None:
+                    rescaled_img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+
+                    images.append(rescaled_img)
+                    labels.append(int(category))
+    return (images, labels)
 
 
 def get_model():
@@ -67,7 +72,40 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.Sequential(
+        [
+            tf.keras.Input(shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+            tf.keras.layers.Conv2D(
+                32,
+                3,
+                strides=(1, 1),
+                padding="valid",
+                dilation_rate=(1, 1),
+                groups=1,
+                activation="relu",
+            ),
+            tf.keras.layers.MaxPool2D(
+                pool_size=(2, 2),
+                strides=None,
+                padding="valid",
+                data_format=None,
+                name=None,
+            ),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dropout(
+                0.5,
+                noise_shape=None,
+                seed=None,
+            ),
+            tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax"),
+        ]
+    )
+    model.compile(
+        optimizer="adam",
+        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+        metrics=["accuracy"],
+    )
+    return model
 
 
 if __name__ == "__main__":
